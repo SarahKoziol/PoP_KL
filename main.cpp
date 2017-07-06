1fd371923cbad79f51959e5ded70fe427550262a
SarahKoziol <sarah.koziol1991@gmail.com>
2017-07-06 22:40:04 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/*
+ * Author: Sarah Koziol, MATRIKELNUMMER
+ * Date: 2017-07-07
+ * Project: Klausur
+ * For: Prozessorientierte Programmierung (Sommersemester 2016)
+ */
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
